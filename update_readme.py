a5a139c16d09ef72139c09804c2806649b7e7629
Ryan M <ryan@ryanmo.co>
2022-12-31 19:03:58 -0700

    clean up commented out code

--- update_readme.py
+++ update_readme.py
@@ -47,31 +47,5 @@ Visit each individual network's pages for show stats:  """.format(now=now.strfti
         TEMPLATE += "\n\n- [{}](networks/{}.md)".format(network.name, quote(network.name.upper()))
         create_network_page(network)
 
-    # TEMPLATE += "\n\n## Table of Contents:"
-    # for network in all_networks:
-    #     network_url = url = re.sub(
-    #         r'[^A-Za-z ]', '', network.name).replace(' ', '-')
-    #     TEMPLATE += "\n- [{0}](#{1})  ".format(network.name, network_url)
-
-    #     for show in network.show_durations.sorted():
-    #         url = re.sub(r'[^A-Za-z ]', '', show.title()).replace(' ', '-')
-    #         TEMPLATE += "\n\t- [{0}](#{1})  ".format(show.title(), url)
-
-    # for network in all_networks:
-    #     TEMPLATE += "\n\n## {}  ".format(network.name)
-
-    #     TEMPLATE += "\n\n**Network's longest episode:** {}".format(
-    #         network.show_durations.get_longest_episode().format())
-    #     TEMPLATE += "\n\n**Network's shortest episode:** {}".format(
-    #         network.show_durations.get_shortest_episode().format())
-
-    #     for show in network.show_durations.sorted():
-    #         TEMPLATE += "\n\n### {}\n\n".format(show.title())
-    #         TEMPLATE += "**Longest episode:** {}  \n".format(
-    #             show.get_longest_episode())
-    #         TEMPLATE += "**Shortest episode:** {}  \n\n".format(
-    #             show.get_shortest_episode())
-    #         TEMPLATE += "![](images/{}.png)".format(quote(show.title()))
-
     with open('README.md', 'w') as f:
         f.write(TEMPLATE)
